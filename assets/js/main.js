a762c69a324e537cdbaf7a67de0526c3af57077b
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-02-21 11:34:17 -0300

    Personal Portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navId);
+
+  if(toggle && nav){
+    toggle.addEventListener('click', ()=>{
+      nav.classList.toggle('show')
+    })
+  }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  navLink.forEach(n => n.classList.remove('active'))
+  this.classList.add('active')
+
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 1400,
+  reset: true
+})
+
+sr.reveal('.home__title',{})
+sr.reveal('.buitton',{delay: 150})
+sr.reveal('.home__img',{delay: 300})
+sr.reveal('.home__social-icon',{interval: 150})
+
+sr.reveal('.about__img',{})
+sr.reveal('.about__subtitle',{delay: 150})
+sr.reveal('.about__text',{delay: 300})
+
+sr.reveal('.skills__subtitle',{})
+sr.reveal('.skills__text',{delay: 150})
+sr.reveal('.skills__data',{interval: 150})
+sr.reveal('.skills__img',{delay: 300})
+
+sr.reveal('.work__img',{delay: 150})
+
+sr.reveal('.contact__input',{delay: 150})
